a8beaf8d17e509caf17f7a5158d6ee1627ed45e2
Moby von Briesen <mobyvb@gmail.com>
2022-08-26 10:50:38 -0400

    cmd,demo: Update help and error text, and add example output

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,6 @@ package cmd
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"time"
 
@@ -30,11 +29,19 @@ var outputtingGrant bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "accesschecker",
-	Short: "Check for empty passphrase in access grant",
-	Long:  `TBD`,
+	Use:   "accesschecker [accessgrant]",
+	Short: "Check for files uploaded with an empty passphrase in a project",
+	Long: `This command is used to determine if you have previously uploaded unencrypted files to a Storj DCS project.
+			To use, go to the Satellite UI, and generate a new access grant for the project you are interested in.
+			Then, run the command with the access you copied from the Satellite UI. It will tell you if you have any unencrypted files uploaded, and what they are called.
+			If you need an (unsafe) "no passphrase" access so that you can download and remove your unencrypted files using uplink, run with the flag "--output-empty-passphrase-access".`,
 	Run: func(cmd *cobra.Command, args []string) {
-		err := beginCheck(args[0])
+		if len(args) == 0 {
+			cmd.Println("You are missing an argument.\n")
+			cmd.Println(cmd.UsageString())
+			return
+		}
+		err := beginCheck(cmd, args[0])
 		if err != nil {
 			panic(err)
 		}
@@ -49,20 +56,20 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolVarP(&outputtingGrant, "output-empty-passphrase-access", "o", false, "Output generated empty passphrase access")
+	rootCmd.Flags().BoolVarP(&outputtingGrant, "output-empty-passphrase-access", "o", false, "Output generated empty passphrase access (only use to download/remove files)")
 }
 
-func beginCheck(accessArg string) error {
+func beginCheck(cmd *cobra.Command, accessArg string) error {
 	ctx := context.Background()
 
 	scope, err := parseAccessRaw(accessArg)
 	if err != nil {
-		return errs.New("error message %+v", err)
+		return errs.New("Could not parse access: %+v", err)
 	}
 
 	apiKey, err := macaroon.ParseRawAPIKey(scope.ApiKey)
 	if err != nil {
-		return errs.New("error message %+v", err)
+		return errs.New("Could not parse API key from access: %+v", err)
 	}
 
 	ident, err := identity.NewFullIdentity(ctx, identity.NewCAOptions{
@@ -70,7 +77,7 @@ func beginCheck(accessArg string) error {
 		Concurrency: 1,
 	})
 	if err != nil {
-		return errs.New("error message %+v", err)
+		return errs.New("Could not create identity to dial satellite: %+v", err)
 	}
 
 	tlsConfig := tlsopts.Config{
@@ -80,45 +87,48 @@ func beginCheck(accessArg string) error {
 
 	tlsOptions, err := tlsopts.NewOptions(ident, tlsConfig, nil)
 	if err != nil {
-		return errs.New("error message %+v", err)
+		return errs.New("Could not create TLS options: %+v", err)
 	}
 
 	dialer := rpc.NewDefaultDialer(tlsOptions)
 	metainfo, err := metaclient.DialNodeURL(ctx, dialer, scope.SatelliteAddr, apiKey, "")
 	if err != nil {
-		return errs.New("error message %+v", err)
+		return errs.New("Error dialing satellite metainfo: %+v", err)
 	}
 	defer func() {
 		err = metainfo.Close()
 		if err != nil {
-			fmt.Printf("error closing metainfo dialer %w\n", err)
+			cmd.Printf("error closing metainfo dialer %w\n", err)
 		}
 	}()
 
 	info, err := metainfo.GetProjectInfo(ctx)
 	if err != nil {
-		return errs.New("error message %+v", err)
+		return errs.New("Error getting project info %+v", err)
 	}
 
 	accessGrant, err := genAccessGrantEmptyPass(scope.SatelliteAddr, apiKey.Serialize(), info.ProjectSalt)
 	if err != nil {
-		return errs.New("error message %+v", err)
+		return errs.New("Error creating an 'empty passphrase' access %+v", err)
 	}
 	keys, n, err := checkFiles(ctx, accessGrant)
 	if err != nil {
-		return errs.New("error message %+v", err)
+		return errs.New("Error checking for unencrypted files: %+v", err)
 	}
 	if n > 0 {
-		if outputtingGrant {
-			fmt.Printf("Generated empty passphrase grant: %s\n", accessGrant)
-		}
-		fmt.Printf("You have %d files uploaded without encryption using this api key:\n", n)
+		cmd.Printf("\nYou have %d files uploaded without encryption in this project:\n\n", n)
 		for _, k := range keys {
-			fmt.Println(k)
+			cmd.Println(k)
+		}
+
+		if outputtingGrant {
+			cmd.Println("\n==================================================\n")
+			cmd.Printf("Generated empty passphrase grant:\n\n%s\n\n", accessGrant)
+			cmd.Println("WARNING: This access is capable of uploading and downloading unencrypted files to and from your project. We recommend using it only to download and subsequently remove files which are unencrypted.")
 		}
 		return nil
 	}
-	fmt.Println("You do not have any files uploaded without encryption using this api key")
+	cmd.Println("You do not have any files uploaded without encryption in this project.")
 	return nil
 }
 
---
+++ demo.md
@@ -0,0 +1,62 @@
+# Window 1
+
+```
+$ accesschecker
+You are missing an argument.
+
+Usage:
+  accesschecker [accessgrant] [flags]
+
+Flags:
+  -h, --help                             help for accesschecker
+  -o, --output-empty-passphrase-access   Output generated empty passphrase access (only use to download/remove files)
+
+$ accesschecker 1QiUipyf19MS5ZMC2y7knDt4jRtf53SgpuXnoX8mdfEqQkx2bUbXTdYhoAxZyioxaWhmjSJR2XHFcBuWJv85oPKSeJ7ZY6XQcUJdRQYrxiG1s6Gf2i6Xxrp1YyAbdG7kythpA6jxr5JBWnMffdioEha9DJWSeHrLUbLcAMTYRjZH1c2yXbg2uxNcAypjbs5dh4Gb4Ksyv4WN7jqQrRqx3eXqf873Z1zFCi8EaczhH4LhEenLgfmUbkL4NAkCRZ8EM5Zp89oP
+
+You have 2 files uploaded without encryption in this project:
+
+sj://asdf/DEVELOPING.md
+sj://moby/README.md
+
+$ accesschecker 1QiUipyf19MS5ZMC2y7knDt4jRtf53SgpuXnoX8mdfEqQkx2bUbXTdYhoAxZyioxaWhmjSJR2XHFcBuWJv85oPKSeJ7ZY6XQcUJdRQYrxiG1s6Gf2i6Xxrp1YyAbdG7kythpA6jxr5JBWnMffdioEha9DJWSeHrLUbLcAMTYRjZH1c2yXbg2uxNcAypjbs5dh4Gb4Ksyv4WN7jqQrRqx3eXqf873Z1zFCi8EaczhH4LhEenLgfmUbkL4NAkCRZ8EM5Zp89oP
+
+You have 2 files uploaded without encryption in this project:
+
+sj://asdf/DEVELOPING.md
+sj://moby/README.md
+moby@thinkpad:~/dev/storj/access-empty-passphrase-checker$ accesschecker 1QiUipyf19MS5ZMC2y7knDt4jRtf53SgpuXnoX8mdfEqQkx2bUbXTdYhoAxZyioxaWhmjSJR2XHFcBuWJv85oPKSeJ7ZY6XQcUJdRQYrxiG1s6Gf2i6Xxrp1YyAbdG7kythpA6jxr5JBWnMffdioEha9DJWSeHrLUbLcAMTYRjZH1c2yXbg2uxNcAypjbs5dh4Gb4Ksyv4WN7jqQrRqx3eXqf873Z1zFCi8EaczhH4LhEenLgfmUbkL4NAkCRZ8EM5Zp89oP --output-empty-passphrase-access
+
+You have 2 files uploaded without encryption in this project:
+
+sj://asdf/DEVELOPING.md
+sj://moby/README.md
+
+==================================================
+
+Generated empty passphrase grant:
+
+1QiUipyf19MS5ZMC2y7knDt4jRtf53SgpuXnoX8mdfEqQkx2bUbXTdYhoAxZyioxaWhmjSJR2XHFcBuWJv85oPKSeJ7ZY6XQcUJdRQYrxiG1s6Gf2i6Xxrp1YyAbdG7kythpA6jxr5JBWnMffdioEha9DJWSeHrLUbLcAMTYRjZH1c2yXbg2uxNcAypjbs5dh4Gb4Ksyv4WN7jqQrRqx3eXqf873Z1zFCi8EaczhH4LhEenLgfmUbkL4NAkCRZ8EM5Zp89oP
+
+WARNING: This access is capable of uploading and downloading unencrypted files to and from your project. We recommend using it only to download and subsequently remove files which are unencrypted.
+
+```
+
+# Window 2
+
+```
+$ uplink access import generatedempty 1QiUipyf19MS5ZMC2y7knDt4jRtf53SgpuXnoX8mdfEqQkx2bUbXTdYhoAxZyioxaWhmjSJR2XHFcBuWJv85oPKSeJ7ZY6XQcUJdRQYrxiG1s6Gf2i6Xxrp1YyAbdG7kythpA6jxr5JBWnMffdioEha9DJWSeHrLUbLcAMTYRjZH1c2yXbg2uxNcAypjbs5dh4Gb4Ksyv4WN7jqQrRqx3eXqf873Z1zFCi8EaczhH4LhEenLgfmUbkL4NAkCRZ8EM5Zp89oP
+Imported access "generatedempty" to "/home/moby/.config/storj/uplink/access.json"
+$ uplink access use generatedempty
+Switched default access to "generatedempty"
+$ uplink rm sj://asdf/DEVELOPING.md
+removed sj://asdf/DEVELOPING.md
+moby@thinkpad:~/dev/storj/storj$ uplink rm sj://moby/README.md
+removed sj://moby/README.md
+```
+
+# Window 1
+
+```
+$ accesschecker 1QiUipyf19MS5ZMC2y7knDt4jRtf53SgpuXnoX8mdfEqQkx2bUbXTdYhoAxZyioxaWhmjSJR2XHFcBuWJv85oPKSeJ7ZY6XQcUJdRQYrxiG1s6Gf2i6Xxrp1YyAbdG7kythpA6jxr5JBWnMffdioEha9DJWSeHrLUbLcAMTYRjZH1c2yXbg2uxNcAypjbs5dh4Gb4Ksyv4WN7jqQrRqx3eXqf873Z1zFCi8EaczhH4LhEenLgfmUbkL4NAkCRZ8EM5Zp89oP --output-empty-passphrase-access
+You do not have any files uploaded without encryption in this project.
+```
