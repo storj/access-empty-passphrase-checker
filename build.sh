2f8937e9987069f8543b000cbc7e94b3dd152922
Maximillian von Briesen <mobyvb@gmail.com>
2022-08-26 15:15:29 -0400

    build.sh: Add file to generate binaries on different OSs (#5)
    This should be the same list of arch/os combinations that Uplink releases have

---
+++ build.sh
@@ -0,0 +1,24 @@
+GOOS=darwin GOARCH=arm64 go build -o bin/accesschecker-arm64-darwin main.go
+GOOS=freebsd GOARCH=amd64 go build -o bin/accesschecker-amd64-freebsd main.go
+GOOS=linux GOARCH=arm go build -o bin/accesschecker-arm-linux main.go
+GOOS=linux GOARCH=arm64 go build -o bin/accesschecker-arm64-linux main.go
+
+# from https://freshman.tech/snippets/go/cross-compile-go-programs/
+# Windows
+# 64-bit
+GOOS=windows GOARCH=amd64 go build -o bin/accesschecker-amd64.exe main.go
+# 32-bit
+GOOS=windows GOARCH=386 go build -o bin/accesschecker-386.exe main.go
+
+# Mac
+# 64-bit
+GOOS=darwin GOARCH=amd64 go build -o bin/accesschecker-amd64-darwin main.go
+# 32-bit
+# has error: "go: unsupported GOOS/GOARCH pair darwin/386"
+# GOOS=darwin GOARCH=386 go build -o bin/accesschecker-386-darwin main.go
+
+# Linux
+# 64-bit
+GOOS=linux GOARCH=amd64 go build -o bin/accesschecker-amd64-linux main.go
+# 32-bit
+GOOS=linux GOARCH=386 go build -o bin/accesschecker-386-linux main.go
