bd8fd69184dca6e34c265ab4bef84d682762a2cd
Maximillian von Briesen <mobyvb@gmail.com>
2022-08-26 16:52:47 -0400

    cmd,demo: Update help text and command output (#6)
    Don't describe files as "unencrypted". Rather, describe them as
    "uploaded with an empty encryption passphrase", because technically
    these files are still encrypted.
    Also, add more os/arch combinations to build script.

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
-GOOS=darwin GOARCH=arm64 go build -o bin/accesschecker-arm64-darwin main.go
-GOOS=freebsd GOARCH=amd64 go build -o bin/accesschecker-amd64-freebsd main.go
-GOOS=linux GOARCH=arm go build -o bin/accesschecker-arm-linux main.go
-GOOS=linux GOARCH=arm64 go build -o bin/accesschecker-arm64-linux main.go
-
-# from https://freshman.tech/snippets/go/cross-compile-go-programs/
+# based partially on https://freshman.tech/snippets/go/cross-compile-go-programs/
 # Windows
 # 64-bit
 GOOS=windows GOARCH=amd64 go build -o bin/accesschecker-amd64.exe main.go
@@ -12,13 +7,19 @@ GOOS=windows GOARCH=386 go build -o bin/accesschecker-386.exe main.go
 
 # Mac
 # 64-bit
-GOOS=darwin GOARCH=amd64 go build -o bin/accesschecker-amd64-darwin main.go
+GOOS=darwin GOARCH=amd64 go build -o bin/accesschecker-darwin-amd64 main.go
+GOOS=darwin GOARCH=arm64 go build -o bin/accesschecker-darwin-arm64 main.go
 # 32-bit
 # has error: "go: unsupported GOOS/GOARCH pair darwin/386"
 # GOOS=darwin GOARCH=386 go build -o bin/accesschecker-386-darwin main.go
 
 # Linux
 # 64-bit
-GOOS=linux GOARCH=amd64 go build -o bin/accesschecker-amd64-linux main.go
+GOOS=linux GOARCH=amd64 go build -o bin/accesschecker-linux-amd64 main.go
+GOOS=linux GOARCH=arm64 go build -o bin/accesschecker-linux-arm64 main.go
 # 32-bit
-GOOS=linux GOARCH=386 go build -o bin/accesschecker-386-linux main.go
+GOOS=linux GOARCH=386 go build -o bin/accesschecker-linux-386 main.go
+GOOS=linux GOARCH=arm go build -o bin/accesschecker-linux-arm main.go
+
+# Free BSD
+GOOS=freebsd GOARCH=amd64 go build -o bin/accesschecker-freebsd-amd64 main.go
